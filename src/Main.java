2a578910de2f4c738741df7523586f119190c3c0
a-statistic <92813931+a-statistic@users.noreply.github.com>
2021-10-19 16:28:58 -0500

    Update

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello GitHub");
+        System.out.println("update to the code!");
+
+    }
+}
