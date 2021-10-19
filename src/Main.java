f58fe96a7d75099c197bcfd2ba5fb0700e196b2f
a-statistic <92813931+a-statistic@users.noreply.github.com>
2021-10-19 17:28:01 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello World");
+        Main mainObject = new Main();
+
+    }
+    private void run(){
+
     }
 }
