f6dcb40bf91548a18caed6c5e05e4d1008ea8cd9
a-statistic <92813931+a-statistic@users.noreply.github.com>
2021-10-19 20:39:26 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.PrintWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -20,13 +21,14 @@ public class Main {
     *@return ArrayList of ints that indicates the number of runs
      */
     private ArrayList<Integer> findRuns(ArrayList<Integer> pList, int pDir){
-        ArrayList<Integer> = arrayListCreate(pList.size(), 0);
+        ArrayList<Integer> runs = arrayListCreate(pList.size(), 0);
         int i = 0;
         int k = 0;
         do{
-            
+            i = pList.size(); //no idea how to implement this code, this is to make it work so I can continue the rest of the program.
 
         } while(i < pList.size());
+        return runs;
     }
     /*
     * merges two lists containing the RUNS_UP and RUNS_DN count
@@ -57,8 +59,28 @@ public class Main {
     * @param pFilename name of file to be written to
     * @param pListRuns the merged list of RUNS_UP and RUNS_down information to be written
      */
-    private void writeOutputFile(String pFilename, ArrayList<Integer> pListRuns){
+    private void writeOutputFile(String pFileName, ArrayList<Integer> pListRuns) throws IOException {
+        PrintWriter out = new PrintWriter(pFileName);
+        int runsTotal = sumArray(pListRuns);
+        out.println("runs_total: " + runsTotal);
+        for(int k = 0; k < pListRuns.size(); k++) {
+            out.println("runs_" + k + ": " + pListRuns.get(k));
+        }
+        out.close();
 
+    }
+    
+    /*
+    *sums all the integers in an ArrayList<Integer>
+    *@param pList an ArrayList<Integer> to be summed
+    * @return the sum of all ints in the ArrayList
+     */
+    private int sumArray(ArrayList<Integer> pList) {
+        int sum = 0;
+        for (int i = 0; i < pList.size(); i++) {
+            sum += pList.get(i);
+        }
+        return sum;
     }
 
     /*
@@ -67,7 +89,15 @@ public class Main {
     * @param pListRuns a list that will record the RUNS data
     * @return an ArrayList<Integer> with the RUNS data
      */
-    private void readInputFile(String pFileName, ArrayList<Integer>  pListRuns){
+    private ArrayList<Integer> readInputFile(String pFileName, ArrayList<Integer>  pListRuns) throws FileNotFoundException {
+        File in = new File(pFileName);
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner read = new Scanner(in);
+        while (read.hasNextInt()) {
+            list.add(read.nextInt());
+        }
+        read.close();
+        return list;
 
     }
 
